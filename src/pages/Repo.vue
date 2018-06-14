6c9bc6c2997ac39f941a0084dc152a24659f7abd
Gustavo Rosario <guqueroz@gmail.com>
2018-06-14 20:57:44 +0000

    Final sem estilo

--- src/pages/Repo.vue
+++ src/pages/Repo.vue
@@ -2,16 +2,25 @@
   <main-layout>
     <h3 class="page-title">Repositório</h3>
     <div v-if="repo.owner" class="row">
-      <h2 class="title">{{repo.name}}</h2>
+      <div class="row">
+        <h2 class="title col s12 m9 l7">{{repo.name}}</h2>
+        <!-- Estrelas -->
+        <div class="starcount amber darken-1 white-text">
+          <i class="material-icons left">star</i>
+          <span> {{repo.stargazers_count}}</span>
+        </div>
+      </div>    
       <h3 class="sub-title">Proprietário: {{repo.owner.login}}</h3>
+      <h3 class="sub-title">Linguagem: <strong>{{repo.language}}</strong></h3>    
       <p>
         {{repo.description || 'Descrição indisponível'}}
       </p>
-      <div class="starcount amber darken-1 white-text">
-        <i class="material-icons left">star</i>
-        <span> {{repo.stargazers_count}}</span>
-      </div>
+      
+      
     </div>
+    <p>
+      Veja informações completas no <a :href="repo.html_url" class="blue-text text-darken-3">GitHub</a>
+    </p>
     
     <!-- LOADER -->
     <div class="progress">
